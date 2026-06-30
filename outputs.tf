7f13077b9f0aad604b585f1b2fbacae15255fc49
Mario Alberto Cañas Baquero <mcanas@unal.edu.co>
2026-06-30 18:30:51 -0500

    Add outputs for production and contingency services
    Added outputs for production and contingency instance groups and health checks for backend service.

--- outputs.tf
+++ outputs.tf
@@ -37,3 +37,25 @@ output "zone" {
   description = "Zona para instancias"
   value       = var.zone
 }
+
+# --- Outputs de Mario (cómputo)
+
+output "mig_produccion_instance_group" {
+  description = "Instance group del MIG de producción (para backend service de Mafe)"
+  value       = google_compute_region_instance_group_manager.mig_produccion.instance_group
+}
+
+output "mig_contingencia_instance_group" {
+  description = "Instance group del MIG de contingencia (para backend service de Mafe)"
+  value       = google_compute_region_instance_group_manager.mig_contingencia.instance_group
+}
+
+output "health_check_produccion_id" {
+  description = "ID del health check de producción (para backend service de Mafe)"
+  value       = google_compute_health_check.hc_produccion.id
+}
+
+output "health_check_contingencia_id" {
+  description = "ID del health check de contingencia (para backend service de Mafe)"
+  value       = google_compute_health_check.hc_contingencia.id
+}
